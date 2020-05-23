3a1b29e58120db2415ba8829c69836a1092d71e4
stevenkcolin <steven.k.colin@gmail.com>
2020-05-24 06:23:41 +0800

    submit

--- contracts/Box.sol
+++ contracts/Box.sol
@@ -2,20 +2,28 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.6.0;
 
+// Import Auth from the access-control subdirectory
+import "./access-control/Auth.sol";
+
 
 contract Box {
     uint256 private value;
+    Auth private auth;
 
-    // Emitted when the stored value changes
     event ValueChanged(uint256 newValue);
 
-    // Stores a new value in the contract
+    constructor(Auth _auth) public {
+        auth = _auth;
+    }
+
     function store(uint256 newValue) public {
+        // Require that the caller is registered as an administrator in Auth
+        require(auth.isAdministrator(msg.sender), "Unauthorized");
+
         value = newValue;
         emit ValueChanged(newValue);
     }
 
-    // Reads the last stored value
     function retrieve() public view returns (uint256) {
         return value;
     }
