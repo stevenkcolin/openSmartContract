223a8f6904a0aaa7635a65306ab5e422b4aef549
stevenkcolin <steven.k.colin@gmail.com>
2020-05-24 08:29:42 +0800

    submit

--- networks.js
+++ networks.js
@@ -1,3 +1,6 @@
+const { projectId, mnemonic } = require('./secrets.json');
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+
 module.exports = {
   networks: {
     development: {
@@ -8,5 +11,12 @@ module.exports = {
       gasPrice: 5e9,
       networkId: '*',
     },
+    ropsten: {
+            provider: () => new HDWalletProvider(
+              mnemonic, 'https://ropsten.infura.io/v3/65ea3ed956c44aca8c46cb48a782712b'
+            ),
+            networkId: 3,
+            gasPrice: 10e9
+          }
   },
 };
